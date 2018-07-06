9f47dbb47e85c1643e7732bc4417139b0cb08f38
Ian Hutchinson <ian@ihut.cc>
2018-07-06 10:09:31 +0100

    Front end tree create and delete

--- webserver/static/app.js
+++ webserver/static/app.js
@@ -2,46 +2,76 @@ $(function() {
   loadLogs();
 
   function loadLogs() {
-    addLog('foo', 'bar', 'baz');
+    $.getJSON('/demoapi/logs/', function(data) {
+      data.logs.forEach(function(log) {
+        console.log(log);
+        addLog(log.name, log.description, log.log_id);
+      });
+    });
   }
 
-  $('#button_new_log').click(function(e) {
+  $('#new_log_button_create').click(function(e) {
     e.preventDefault();
 
     var name = $('#new_log_name').val();
+    var description = $('#new_log_description').val();
 
-    if (name === '') {
+    if (name === '' || description === '') {
+      // TODO: Add error message
       return;
     }
 
-    console.log('create new log with name:', name);
+    $.ajax({
+      type: 'POST',
+      url: '/demoapi/logs/',
+      dataType: 'json',
+      contentType: 'application/json',
+      async: false,
+      data: JSON.stringify({
+        name: name,
+        description: description,
+      }),
+      success: function(data) {
+        console.log(data);
 
-    // On success
-    $('#new_log_name').val('');
+        $('#new_log_name').val('');
+        $('#new_log_description').val('');
+      },
+    });
   });
 
   $('body').on('click', '.delete_log', function(e) {
     e.preventDefault();
 
-    var uid = $(this).parent().parent().attr('data-uid');
+    var id = $(this).parent().parent().attr('data-id');
 
-    $(this).parent().parent().remove();
+    console.log('/demoapi/logs/' + id + '/');
+
+    $.ajax({
+      url: '/demoapi/logs/' + id + '/',
+      type: 'DELETE',
+      success: function(result) {
+        console.log(result);
+      }
+    });
+
+    //$(this).parent().parent().remove();
   });
 
-  function addLog(friendlyName, slug, uid) {
+  function addLog(name, description, id) {
     var li = $('<li>');
 
-    li.attr('data-uid', uid);
+    li.attr('data-id', id);
 
-    var pFriendlyName = $('<p>');
-    pFriendlyName.addClass('bold');
-    pFriendlyName.text(friendlyName);
+    var pName = $('<p>');
+    pName.addClass('bold');
+    pName.text(name);
 
-    var pSlug = $('<p>');
-    pSlug.text(slug);
+    var pDescription = $('<p>');
+    pDescription.text(description);
 
-    var pUniqueId = $('<p>');
-    pUniqueId.text(uid);
+    var pId = $('<p>');
+    pId.text(id);
 
     var pButtons = $('<p>');
 
@@ -50,12 +80,11 @@ $(function() {
     aDelete.addClass('delete_log');
     aDelete.text('Delete');
 
-
     pButtons.append(aDelete);
 
-    li.append(pFriendlyName);
-    li.append(pSlug);
-    li.append(pUniqueId);
+    li.append(pName);
+    li.append(pDescription);
+    li.append(pId);
     li.append(pButtons);
 
     $('#current_logs').append(li);
